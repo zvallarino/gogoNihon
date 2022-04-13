b70b1a47df05af52cf1e430072dcf8a08ce8bac1
zachary vallarino <zvcoding@gmail.com>
2022-04-13 12:07:28 -0500

    React setup

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Page Count: {count}</h1>
+    </div>
+  );
+}
+export default App;
